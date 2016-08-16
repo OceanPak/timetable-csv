318ae9a05b0b4e850c4d656fc4d6d7b311f47f28
Adam <sg6009@student.cis.edu.hk>
2016-08-16 23:57:58 +0800

    Add logic to generatecsv.js to create cycle day events

--- public/javascripts/generatecsv.js
+++ public/javascripts/generatecsv.js
@@ -7,11 +7,11 @@ module.exports = function(timetable, cdates) {
     ['11:35:00', '12:50:00'],
     ['13:45:00', '15:00:00']
   ];
-  
+
   var raw = timetable.split("\r\n");
-  
+
   var splitday = [];
-  
+
   var c = 0;
   for (var i = 1; i < raw.length; i++) {
     if (raw[i].indexOf('\t') > -1) {
@@ -20,21 +20,39 @@ module.exports = function(timetable, cdates) {
     }
   }
   splitday.push(raw.slice(c, raw.length));
-  
+
   var masterstr = '';
-  masterstr += 'Subject,Start Date,Start Time,End Date,End Time,Location,Description\n';
-  
+  masterstr += 'Subject,Start Date,Start Time,End Date,End Time,Location,Description,All Day Event\n';
+
   for (i = 0; i < splitday.length; i++) {
     var day = splitday[i];
-    
-    var dates = cdates[day[0].split(' ')[1].slice(0, 2)];
-    
+
+		var sday = day[0].split(' ')[1].slice(0, 2);
+
+    var dates = cdates[sday];
+
+		for (var j = 0; j < dates.length; j++) {
+			var d = dates[j];
+			
+			var subject = 'Day ' + sday;
+			var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
+			var startTime = '';
+			var endDate = startDate;
+			var endTime = '';
+			var location = '';
+			var description = '';
+			var allDayEvent = 'True';
+
+			var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
+			masterstr += s;
+		}
+
     day.splice(0, 1);
-    
+
     var classes = [];
-    
+
     var k = 0;
-    for (var j = 0; j < 6; j++) {
+    for (j = 0; j < 6; j++) {
       if (day[k] != '') {
         classes[j] = day.slice(k, k + 3);
         k += 3;
@@ -42,12 +60,12 @@ module.exports = function(timetable, cdates) {
         k += 2;
       }
     }
-    
+
     for (j = 0; j < dates.length; j++) {
       for (k = 0; k < classes.length; k++) {
         var d = dates[j];
         var c = classes[k];
-        
+
         if (c != undefined) {
           var subject = c[0];
           var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
@@ -56,13 +74,14 @@ module.exports = function(timetable, cdates) {
           var endTime = periodtimes[k][1];
           var location = c[2];
           var description = c[1];
-          
-          var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + '\n';
+					var allDayEvent = 'False';
+
+          var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
           masterstr += s;
         }
       }
     }
   }
-  
+
   return masterstr;
 };
