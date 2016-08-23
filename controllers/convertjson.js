552782c170e77440327d96dab0be7c36e53d44c5
Adam <adamexcaliburguo@gmail.com>
2016-08-23 22:32:28 +0800

    Move midterm-checking logic from generatecsv to convertjson

--- controllers/convertjson.js
+++ controllers/convertjson.js
@@ -1,12 +1,19 @@
 module.exports = function(jdates) {
 	var cdates = {};
 
+	// stop-midterm
+	var midterm = new Date(2016, 9, 24);
+
 	for (var key in jdates) {
 		var timestamps = jdates[key];
 		var dates = [];
 
 		for (var i = 0; i < timestamps.length; i++) {
-			dates.push(new Date(timestamps[i]));
+			var d = new Date(timestamps[i]);
+			// stop-midterm
+			if (d.valueOf() < midterm.valueOf()) {
+				dates.push(d);
+			}
 		}
 
 		cdates[key] = dates;
--- controllers/generatecsv.js
+++ controllers/generatecsv.js
@@ -1,8 +1,5 @@
 module.exports = function(timetable, cdates, activities) {
 
-	// stop-midterm
-	var midterm = new Date(2016, 9, 24);
-
 	var periodtimes = [
     ['07:55:00', '09:15:00'],
     ['09:20:00', '10:35:00'],
@@ -37,22 +34,19 @@ module.exports = function(timetable, cdates, activities) {
 	    var dates = cdates[sday];
 
 			for (var j = 0; j < dates.length; j++) {
-				// stop mid-term
-				if (dates[j].valueOf() < midterm.valueOf()) {
-					var d = dates[j];
+				var d = dates[j];
 
-					var subject = 'Day ' + sday;
-					var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
-					var startTime = '';
-					var endDate = startDate;
-					var endTime = '';
-					var location = '';
-					var description = '';
-					var allDayEvent = 'True';
+				var subject = 'Day ' + sday;
+				var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
+				var startTime = '';
+				var endDate = startDate;
+				var endTime = '';
+				var location = '';
+				var description = '';
+				var allDayEvent = 'True';
 
-					var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
-					masterstr += s;
-				}
+				var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
+				masterstr += s;
 			}
 
 	    day.splice(0, 1);
@@ -70,27 +64,24 @@ module.exports = function(timetable, cdates, activities) {
 	    }
 
 	    for (j = 0; j < dates.length; j++) {
-				// stop-midterm
-				if (dates[j].valueOf() < midterm.valueOf()) {
-					for (k = 0; k < classes.length; k++) {
-		        var d = dates[j];
-		        var c = classes[k];
+				for (k = 0; k < classes.length; k++) {
+	        var d = dates[j];
+	        var c = classes[k];
 
-		        if (c != undefined) {
-		          var subject = c[0];
-		          var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
-		          var startTime = periodtimes[k][0];
-		          var endDate = startDate;
-		          var endTime = periodtimes[k][1];
-		          var location = c[2];
-		          var description = c[1];
-							var allDayEvent = 'False';
+	        if (c != undefined) {
+	          var subject = c[0];
+	          var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
+	          var startTime = periodtimes[k][0];
+	          var endDate = startDate;
+	          var endTime = periodtimes[k][1];
+	          var location = c[2];
+	          var description = c[1];
+						var allDayEvent = 'False';
 
-		          var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
-		          masterstr += s;
-		        }
-		      }
-				}
+	          var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
+	          masterstr += s;
+	        }
+	      }
 	    }
 	  }
 	}
@@ -114,22 +105,19 @@ module.exports = function(timetable, cdates, activities) {
 			var dates = cdates[activity.day.toUpperCase()];
 
 			for (var j = 0; j < dates.length; j++) {
-				// stop-midterm
-				if (dates[j].valueOf() < midterm.valueOf()) {
-					d = dates[j];
+				d = dates[j];
 
-					var subject = activity.name;
-					var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
-					var startTime = activitiesPeriodTimes[activity.startPeriod][0];
-					var endDate = startDate;
-					var endTime = activitiesPeriodTimes[activity.endPeriod][1];
-					var location = activity.location;
-					var description = '';
-					var allDayEvent = 'False';
+				var subject = activity.name;
+				var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
+				var startTime = activitiesPeriodTimes[activity.startPeriod][0];
+				var endDate = startDate;
+				var endTime = activitiesPeriodTimes[activity.endPeriod][1];
+				var location = activity.location;
+				var description = '';
+				var allDayEvent = 'False';
 
-					var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
-					masterstr += s;
-				}
+				var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
+				masterstr += s;
 			}
 		} else {
 			var keys = Object.keys(cdates);
@@ -138,23 +126,20 @@ module.exports = function(timetable, cdates, activities) {
 				var dates = cdates[keys[j]];
 
 				for (var k = 0; k < dates.length; k++) {
-					// stop-midterm
-					if (dates[k].valueOf() < midterm.valueOf()) {
-						d = dates[k];
+					d = dates[k];
 
-						if (d.getDay() == activity.day) {
-							var subject = activity.name;
-							var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
-							var startTime = activitiesPeriodTimes[activity.startPeriod][0];
-							var endDate = startDate;
-							var endTime = activitiesPeriodTimes[activity.endPeriod][1];
-							var location = activity.location;
-							var description = '';
-							var allDayEvent = 'False';
+					if (d.getDay() == activity.day) {
+						var subject = activity.name;
+						var startDate = '' + (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
+						var startTime = activitiesPeriodTimes[activity.startPeriod][0];
+						var endDate = startDate;
+						var endTime = activitiesPeriodTimes[activity.endPeriod][1];
+						var location = activity.location;
+						var description = '';
+						var allDayEvent = 'False';
 
-							var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
-							masterstr += s;
-						}
+						var s = subject + ',' + startDate + ',' + startTime + ',' + endDate + ',' + endTime + ',' + location + ',' + description + ',' + allDayEvent + '\n';
+						masterstr += s;
 					}
 				}
 			}
