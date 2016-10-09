397b83899773a5fd980074baa667ceb81f5edb1c
Adam <adamexcaliburguo@gmail.com>
2016-10-09 19:43:13 +0800

    Change to create events on 9/9/2016 onwards

--- controllers/convertjson.js
+++ controllers/convertjson.js
@@ -6,14 +6,17 @@ module.exports = function() {
 	// stop-midterm
 	var midterm = new Date(2016, 9, 24);
 
+	// start-midterm
+	var startMidterm = new Date(2016, 9, 9);
+
 	for (var key in jdates) {
 		var timestamps = jdates[key];
 		var dates = [];
 
 		for (var i = 0; i < timestamps.length; i++) {
 			var d = new Date(timestamps[i]);
-			// stop-midterm
-			if (d.valueOf() < midterm.valueOf()) {
+			// start-midterm
+			if (d.valueOf() > startMidterm.valueOf()) {
 				dates.push(d);
 			}
 		}
