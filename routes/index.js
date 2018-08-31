39c351421a7aacfbac1c813a78ae8e7e1864765e
Chung Hang Pak (Ocean) <chp026@student.cis.edu.hk>
2018-09-01 00:19:32 +0800

    Commenting

--- routes/index.js
+++ routes/index.js
@@ -34,7 +34,6 @@ router.get('/', function(req, res, next) {
 router.post('/getcsv', function(req, res, next) {
 
 	// console.log(JSON.stringify(req.body.timetable));
-	// test
 	if (req.body.timetable === '') {
 
 		var allEvents = [
